d9fe560100a7c1c2dda89d5dec48b376c913e797
YuliaPortnova <yuyul@yandex.ru>
2024-04-20 20:45:01 +0300

    feat: добавляет каталог для планшета

--- source/scripts/index.js
+++ source/scripts/index.js
@@ -66,16 +66,22 @@ initSlider();
 const initPrice = () => {
   const MIN_PRICE = 0;
   const START_MAX_PRICE = 900;
-  const MAX_PRICE = 977;
+  const MAX_PRICE_MOBILE = 977;
+  const MAX_PRICE_TABLET = 1000;
 
   const slider = document.querySelector('.form__slider');
   const inputMinPrice = document.querySelector('.form__input--min-price');
   const inputMaxPrice = document.querySelector('.form__input--max-price');
 
+  let maxPrice = MAX_PRICE_MOBILE;
+  if (document.documentElement.clientWidth > 768) {
+    maxPrice = MAX_PRICE_TABLET;
+  }
+
   noUiSlider.create(slider, {
     range: {
       'min': [MIN_PRICE],
-      'max': [MAX_PRICE],
+      'max': [maxPrice],
     },
     start: [MIN_PRICE, START_MAX_PRICE],
     step: 1,
