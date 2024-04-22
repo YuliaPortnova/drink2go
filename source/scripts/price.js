bfa5c9a1815a4dfbd677f33ef357a1804a7271df
YuliaPortnova <yuyul@yandex.ru>
2024-04-22 23:10:10 +0300

    feat: приводит максимально возможную цену в соответствие с макетом

--- source/scripts/price.js
+++ source/scripts/price.js
@@ -2,6 +2,7 @@ const MIN_PRICE = 0;
 const START_MAX_PRICE = 900;
 const MAX_PRICE_MOBILE = 977;
 const MAX_PRICE_TABLET = 1000;
+const MAX_PRICE_DESKTOP = 966;
 
 const slider = document.querySelector('.form__slider');
 const inputMinPrice = document.querySelector('.form__input--min-price');
@@ -11,16 +12,24 @@ document.head.insertAdjacentHTML(
   'beforeend',
   '<link rel="stylesheet" href="vendor/nouislider/nouislider.css">');
 
-let maxPrice = MAX_PRICE_MOBILE;
-if (document.documentElement.clientWidth > 768) {
-  maxPrice = MAX_PRICE_TABLET;
-}
+const getSliderRange = (width) => {
+  let maxPrice;
+  if (width < 768) {
+    maxPrice = MAX_PRICE_MOBILE;
+  } else if (width < 1440) {
+    maxPrice = MAX_PRICE_TABLET;
+  } else {
+    maxPrice = MAX_PRICE_DESKTOP;
+  }
 
-noUiSlider.create(slider, {
-  range: {
+  return {
     'min': [MIN_PRICE],
     'max': [maxPrice],
-  },
+  };
+};
+
+noUiSlider.create(slider, {
+  range: getSliderRange(window.innerWidth),
   start: [MIN_PRICE, START_MAX_PRICE],
   step: 1,
   connect: true,
@@ -43,6 +52,12 @@ const initPrice = () => {
 
   inputMaxPrice.addEventListener('input', () => {
     slider.noUiSlider.set([inputMinPrice.value, inputMaxPrice.value]);
+  });
+
+  window.addEventListener('resize', () => {
+    slider.noUiSlider.updateOptions({
+      range: getSliderRange(window.innerWidth)
+    });
   });
 };
 
